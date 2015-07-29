1905007403ad33357a34f2674ab3ec09f7745713
Ian Dundas <contact@iandundas.co.uk>
2015-07-29 16:51:14 +0200

    Added a List model object

---
+++ CoreDataCloudKit/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  CoreDataCloudKit
+//
+//  Created by Ian Dundas on 29/07/2015.
+//  Copyright (c) 2015 Ian Dundas. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Item: NSManagedObject {
+
+    @NSManaged var title: String
+
+}
