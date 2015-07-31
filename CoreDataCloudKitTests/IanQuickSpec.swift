d34fdca2c6ec87ca7918895090202fd8084f7019
Ian Dundas <contact@iandundas.co.uk>
2015-07-31 23:25:15 +0200

    Check that Quick is functioning

---
+++ CoreDataCloudKitTests/IanQuickSpec.swift
@@ -0,0 +1,10 @@
+import Quick
+import Nimble
+
+class IanQuickSpec: QuickSpec {
+    override func spec() {
+        it("is working at all") {
+            expect(1 + 1).to(equal(2))
+        }
+    }
+}
