3c9cbe30e84fee926d243a7b8cf7957a1739f6e6
Ian Dundas <contact@iandundas.co.uk>
2015-07-31 23:39:43 +0200

    Added frivolous extensiosn

--- CoreDataCloudKit/main.swift
+++ CoreDataCloudKit/main.swift
@@ -16,3 +16,33 @@ if isRunningTests {
 } else {
    UIApplicationMain(Process.argc, Process.unsafeArgv, nil, NSStringFromClass(AppDelegate))
 }
+
+
+func when(@autoclosure test:  () -> Bool, action: () -> ()) {
+    if test() { action() }
+}
+
+func unless(@autoclosure test:  () -> Bool, action: () -> ()) {
+    if !test() { action() }
+}
+
+
+// MARK These almost certainly shouldn't be here - move when I've learned where to put them
+// (they don't work in any other swift file :S
+
+// https://gist.github.com/JadenGeller/2828e1f7458c8b41a7b0
+when(3 == 5){
+    println("three equals five")         // Will print!
+}
+
+unless(3 == 5){
+    println("three does not equal five") // Will never print.
+}
+
+when(1 == 1){
+    println("one equals one")            // Will print!
+}
+
+unless(1 == 1){
+    println("one does not equal one")    // Will never print.
+}
